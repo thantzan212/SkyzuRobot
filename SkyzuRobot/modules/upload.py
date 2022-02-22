d41edcd314b0f0f429bfc7b31bd81f389998d3da
thant <90088844+thantzan212@users.noreply.github.com>
2022-02-22 17:00:52 +0630

    Update upload.py

--- SkyzuRobot/modules/upload.py
+++ SkyzuRobot/modules/upload.py
@@ -118,7 +118,7 @@ async def tmp(event):
 
             str(time.time() - start)
             await orta.edit(
-                f"File Successfully Uploaded to TmpNinja.\n\nLink 👉 {download_link}\n\nUploaded by *AsunaRobot*"
+                f"File Successfully Uploaded to TmpNinja.\n\nLink 👉 {download_link}\n\nUploaded by *Botညီလေး*"
             )
         except Exception as e:
             traceback.print_exc()
@@ -159,7 +159,7 @@ async def up(event):
                 event.chat.id,
                 dosya,
                 force_document=True,
-                caption=f"Uploaded By *AsunaRobot*",
+                caption=f"Uploaded By *Botညီလေး*",
             )
         except Exception as e:
             traceback.print_exc()
